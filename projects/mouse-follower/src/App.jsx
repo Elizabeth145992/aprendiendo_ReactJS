eaa6f742cf111ff2b97ae491707e414687ac8867
Elizabeth145992 <e.cruzramirez@ugto.mx>
2025-05-06 21:02:43 -0600

    add new projecto to learn useEffect

---
+++ projects/mouse-follower/src/App.jsx
@@ -0,0 +1,58 @@
+import './App.css'
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({x:0, y:0});
+
+  //Cambio de posiciones del cursor para seguir la bolita
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY} = event;
+      setPosition({x: clientX, y: clientY});
+    }
+
+    if(enabled){
+      window.addEventListener('pointermove', handleMove);
+    }
+
+    //Se ejecuta cuando el domponente se desmonta
+    //Se ejecuta cuando cambian las dependencias, antes de ejecutar el efecto de neuvo
+    return () => {
+      window.removeEventListener('pointermove', handleMove);
+    }
+  }, [enabled])
+
+  //cambiar estilo del cursor
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+  return (
+    <>
+    <main>
+      <div style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`
+      }} />
+      <button onClick={()=>setEnabled(!enabled)}>
+        {enabled ? 'Desactivar ': 'Activar '} seguir puntero
+      </button>
+    </main>
+    </>
+  )
+}
+
+export default App
