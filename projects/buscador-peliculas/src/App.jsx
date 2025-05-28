5ceb3f6aed00bb3e34709707cb51be8cb022dc36
Elizabeth145992 <e.cruzramirez@ugto.mx>
2025-05-27 20:50:27 -0600

    add hook and components in aplication

--- projects/buscador-peliculas/src/App.jsx
+++ projects/buscador-peliculas/src/App.jsx
@@ -1,41 +1,54 @@
-import './App.css';
-import results from './mocks/data.json';
-import noResults from './mocks/no-results.json';
+import { useState } from 'react';
+import "./App.css";
+//import { useRef } from 'react';
+import { Movies } from "./components/Movies";
+import { useMovies } from "./hooks/useMovies";
 
 function App() {
-  const movies = results.Search;
-  const hasMovies = movies?.length > 0;
+  const { movies } = useMovies();
+  const [ query, setQuery] = useState('');
+  //const inputRef = useRef();
+
+  //Usando el hook de Ref
+  /*const handleSubmit = (event) =>{
+    event.preventDefault();
+    const inputValue = inputRef.current.value;
+  }*/
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(query);
+  };
+
+  const handleChange = (event) =>{
+    setQuery(event.target.value);
+  }
+  
 
   return (
     <>
-    <div>
-      <header className='page'>
-        <h1>Buscador de películas</h1>
-        <form className='form'>
-          <input type="text" placeholder='Busca tu pelicula favorita' />
-          <button type="submit">Buscar</button>
-        </form>
-      </header>
+      <div>
+        <header className="page">
+          <h1>Buscador de películas</h1>
+          <form className="form" onSubmit={handleSubmit}>
+            <input
+              /*ref={inputRef}*/
+              value={query}
+              onChange={handleChange}
+              name="query"
+              type="text"
+              placeholder="Busca tu pelicula favorita"
+            />
+            <button type="submit">Buscar</button>
+          </form>
+        </header>
 
-      <main>
-        {
-        hasMovies ?(
-          <ul>
-            {movies.map(movie => (
-              <li key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
-                <img src={movie.Poster} alt={movie.Title} />
-              </li>
-            ))}
-          </ul>
-        ):
-        (<p>No se encontaron películas</p>)
-        }
-      </main>
-    </div>
+        <main>
+          <Movies movies={movies} />
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ projects/buscador-peliculas/src/hooks/useMovies.js
@@ -0,0 +1,12 @@
+import results from '../mocks/data.json';
+export function useMovies(){
+  const movies = results.Search;
+  const mappedMovies = movies?.map(movie => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    poster: movie.Poster
+  }))
+
+  return {movies: mappedMovies}
+}
